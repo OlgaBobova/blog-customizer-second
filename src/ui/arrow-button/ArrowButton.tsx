e4c51bc69870d619602e7b053fb0ab552af2d098
Olga Bobova <bobchik2021@yandex.ru>
2024-12-16 14:30:11 +0300

    первое ревью

--- src/ui/arrow-button/ArrowButton.tsx
+++ src/ui/arrow-button/ArrowButton.tsx
@@ -6,24 +6,28 @@ import clsx from 'clsx';
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-type ArrowButtonProps = {
+export type ArrowButtonProps = {
+	onClick?: OnClick;
 	isOpen: boolean;
-	onClick: OnClick;
 };
 
-export const ArrowButton = ({ isOpen, onClick }: ArrowButtonProps) => {
+export const ArrowButton = ({ onClick, isOpen }: ArrowButtonProps) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
+			onClick={onClick}
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={clsx(styles.container, { [styles.container_open]: isOpen })}
-			onClick={onClick}>
+			className={clsx(styles.container, {
+				[styles.container_open]: isOpen,
+			})}>
 			<img
 				src={arrow}
 				alt='иконка стрелочки'
-				className={clsx(styles.arrow, { [styles.arrow_open]: isOpen })}
+				className={clsx(styles.arrow, {
+					[styles.arrow_open]: isOpen,
+				})}
 			/>
 		</div>
 	);
